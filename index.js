9264e53cc8d3e283564f7d87b36edd1424d3d7d2
Alex Maciel <alexmaciel.mail@gmail.com>
2021-07-27 19:00:57 -0300

    integracao com mysql

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+const connection  = require('./database/database')
+const Pergunta = require('./database/Pergunta')
 
-
+connection.authenticate()
+.then(()=>{
+    console.log("Conexão realizada")
+})
+.catch((e)=>{
+    console.log('Erro: ' + e)
+})
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
@@ -22,6 +30,15 @@ app.post("/salvarpergunta", (req, res)=>{
     var titulo = req.body.titulo;
     var descricao = req.body.descricao;
 
-    res.send(`${titulo} - ${descricao}`)
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(()=>{
+        res.redirect("/")
+    }).catch(()=>{
+        res.send(`Falha ao cadastrar pergunta!`)
+    })
+
+    
 })
 app.listen(8081, ()=>{console.log('App rodando!')})
